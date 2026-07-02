c37135315f63c6144c2fc65e2e124dd1985b3840
Shaik Haider <shaikhaider2027@gmail.com>
2026-07-02 10:13:17 +0530

    Update Calculator.java

--- WEEK-3/src/main/java/com/example/Calculator.java
+++ WEEK-3/src/main/java/com/example/Calculator.java
@@ -1,10 +1,6 @@
 package com.example;
 
-/**
- * A small class with a couple of methods, used to demonstrate
- * setting up JUnit and writing basic tests (Exercise 1) and to
- * exercise the Arrange-Act-Assert pattern (Exercise 4).
- */
+
 public class Calculator {
 
     public int add(int a, int b) {
